7a8fdd5dd59566ecfb54d136ad0ee39f6a80f88b
ivan <ivan.mishakivskiy@gearheart.io>
2024-02-08 19:07:31 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -73,7 +73,7 @@ function App() {
             <li className='list__item'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" class="h-6 w-6 text-green-500 mr-4"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clip-rule="evenodd"></path></svg>
               <p className='list__item-text'>
-                Підходить до будь-яких поверхонь
+                Підходить для будь-яких поверхонь
               </p>
             </li>
             <li className='list__item'>
